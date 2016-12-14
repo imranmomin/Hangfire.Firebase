6b9267b30c3b873eb603d385aae3630b765343ae
imranmomin <imran.amomin@outlook.com>
2016-12-14 14:09:55 -0500

    created FirebaseStorageOption

---
+++ Hangfire.Firbase/FirebaseStorageOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hangfire.Firbase
+{
+    public class FirebaseStorageOptions
+    {
+        public string AuthSecret { get; set; }
+        public TimeSpan? RequestTimeout { get; set; }
+        public bool PrepareSchema { get; set; }
+
+        public FirebaseStorageOptions()
+        {
+            RequestTimeout = TimeSpan.FromSeconds(30);
+            PrepareSchema = true;
+        }
+    }
+}
