872c4c1bb16b2d83b9b24600e9fc47132d08ce41
imranmomin <imran.amomin@outlook.com>
2016-12-14 14:25:41 -0500

    created FirebaseStorage extension

---
+++ Hangfire.Firbase/FirebaseStorageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Hangfire.Firbase;
+
+namespace Hangfire
+{
+    public static class FirebaseStorageExtensions
+    {
+        public static IGlobalConfiguration<FirebaseStorage> UseSqlServerStorage(this IGlobalConfiguration configuration, string url, string authSecret)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
+            if (string.IsNullOrEmpty(authSecret)) throw new ArgumentNullException(nameof(authSecret));
+
+            var storage = new FirebaseStorage(url, authSecret);
+            return configuration.UseStorage(storage);
+        }
+
+        public static IGlobalConfiguration<FirebaseStorage> UseSqlServerStorage(this IGlobalConfiguration configuration, string url, FirebaseStorageOptions options)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            if (string.IsNullOrEmpty(url)) throw new ArgumentNullException(nameof(url));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            var storage = new FirebaseStorage(url, options);
+            return configuration.UseStorage(storage);
+        }
+    }
+}
