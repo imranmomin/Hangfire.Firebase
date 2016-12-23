ecd934ae7a86c9852844180f54af819023460fbe
imranmomin <imran.amomin@outlook.com>
2016-12-23 11:52:32 -0500

    update the server dto

--- Hangfire.Firebase/Entities/Server.cs
+++ Hangfire.Firebase/Entities/Server.cs
@@ -5,7 +5,9 @@ namespace Hangfire.Firebase.Entities
     internal class Server
     {
         public string Id { get; set; }
-        public string Name { get; set; }
+        public int Workers { get; set; }
+        public string[] Queues { get; set; }
+        public DateTime CreatedOn { get; set; }
         public DateTime LastHeartbeat { get; set; }
     }
 }
