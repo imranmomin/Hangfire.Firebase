b3ec4f9f5a9695f862d4d7f41d07480683ed0a6c
Imran Momin <360iam@gmail.com>
2016-12-24 12:17:37 -0500

    StateId for the job

--- Hangfire.Firebase/Entities/Job.cs
+++ Hangfire.Firebase/Entities/Job.cs
@@ -7,6 +7,7 @@ namespace Hangfire.Firebase.Entities
     {
         public InvocationData InvocationData { get; set; }
         public string Arguments { get; set; }
+        public string StateId { get; set; }
         public string StateName { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime ExpireOn { get; set; }
