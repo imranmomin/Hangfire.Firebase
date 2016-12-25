947842bf0a7e698687b140f0026f6c76bbd42e1f
Imran Momin <360iam@gmail.com>
2016-12-24 20:20:52 -0500

    jsone serializer

---
+++ Hangfire.Firebase/Json/JsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using FireSharp.Interfaces;
+
+namespace Hangfire.Firebase.Json
+{
+    internal class JsonSerializer : ISerializer
+    {
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            ContractResolver = new LowerCaseDelimitedPropertyNamesContractResovler()
+        };
+
+        public T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json, settings);
+        public string Serialize<T>(T value) => JsonConvert.SerializeObject(value, settings);
+    }
+}
