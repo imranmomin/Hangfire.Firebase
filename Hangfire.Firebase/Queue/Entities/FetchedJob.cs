f66d9ef71bab6466f45c7bc0bacf740376d4c9dd
imranmomin <imran.amomin@outlook.com>
2016-12-29 16:00:28 -0500

    fixed the url and added refernce to queue

--- Hangfire.Firebase/Queue/Entities/FetchedJob.cs
+++ Hangfire.Firebase/Queue/Entities/FetchedJob.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
-using FireSharp.Response;
+
 using Hangfire.Storage;
+using FireSharp.Response;
 
 namespace Hangfire.Firebase.Queue
 {
@@ -8,12 +9,15 @@ namespace Hangfire.Firebase.Queue
     {
         private FirebaseConnection connection;
 
-        public FetchedJob(FirebaseStorage storage, string queue, string jobId)
+        public FetchedJob(FirebaseStorage storage, string queue, string jobId, string reference)
         {
             this.connection = (FirebaseConnection)storage.GetConnection();
             JobId = jobId;
             Queue = queue;
+            Reference = reference;
         }
+
+        private string Reference { get; set; }
 
         public string JobId { get; }
 
@@ -21,17 +25,16 @@ namespace Hangfire.Firebase.Queue
 
         public void Dispose()
         {
-            connection = null;
         }
 
-        public void RemoveFromQueue() => connection.Client.Delete($"queue/${Queue}/${JobId}");
+        public void RemoveFromQueue() => connection.Client.Delete($"queue/{Queue}/{Reference}");
 
         public void Requeue()
         {
-            FirebaseResponse response = connection.Client.Get($"queue/${Queue}/${JobId}");
+            FirebaseResponse response = connection.Client.Get($"queue/{Queue}/{Reference}");
             if (response.StatusCode == HttpStatusCode.NotFound)
             {
-                connection.Client.Push($"queue/${Queue}", JobId);
+                connection.Client.Push($"queue/{Queue}", JobId);
             }
         }
     }
