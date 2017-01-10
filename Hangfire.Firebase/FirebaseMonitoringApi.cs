169afb2e1bd9573e78abc0e0599d89313a9ec99b
imranmomin <imran.amomin@outlook.com>
2017-01-10 16:11:48 -0500

    completed some stats methods

--- Hangfire.Firebase/FirebaseMonitoringApi.cs
+++ Hangfire.Firebase/FirebaseMonitoringApi.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using FireSharp;
 using Hangfire.Storage;
+using FireSharp.Response;
+using Hangfire.Firebase.Queue;
+using Hangfire.Firebase.Entities;
 using Hangfire.Storage.Monitoring;
 
 namespace Hangfire.Firebase
 {
-   public sealed class FirebaseMonitoringApi : IMonitoringApi
+    public sealed class FirebaseMonitoringApi : IMonitoringApi
     {
+        private readonly FirebaseConnection connection;
+        private readonly FirebaseStorage storage;
+
+        public FirebaseMonitoringApi(FirebaseStorage storage)
+        {
+            this.storage = storage;
+            connection = (FirebaseConnection)storage.GetConnection();
+        }
+
         public IList<QueueWithTopEnqueuedJobsDto> Queues()
         {
             throw new NotImplementedException();
@@ -65,59 +79,86 @@ namespace Hangfire.Firebase
             throw new NotImplementedException();
         }
 
-        public long ScheduledCount()
-        {
-            throw new NotImplementedException();
-        }
+        #region Counts
+
+        public long ScheduledCount() => GetNumberOfJobsByStateName(States.ScheduledState.StateName);
 
         public long EnqueuedCount(string queue)
         {
-            throw new NotImplementedException();
+            IPersistentJobQueueProvider provider = storage.QueueProviders.GetProvider(queue);
+            IPersistentJobQueueMonitoringApi monitoringApi = provider.GetJobQueueMonitoringApi();
+            return monitoringApi.GetEnqueuedCount(queue);
         }
 
         public long FetchedCount(string queue)
         {
-            throw new NotImplementedException();
+            IPersistentJobQueueProvider provider = storage.QueueProviders.GetProvider(queue);
+            IPersistentJobQueueMonitoringApi monitoringApi = provider.GetJobQueueMonitoringApi();
+            return monitoringApi.GetEnqueuedCount(queue);
         }
 
-        public long FailedCount()
+        public long FailedCount() => GetNumberOfJobsByStateName(States.FailedState.StateName);
+
+        public long ProcessingCount() => GetNumberOfJobsByStateName(States.ProcessingState.StateName);
+
+        public long SucceededListCount() => GetNumberOfJobsByStateName(States.SucceededState.StateName);
+
+        public long DeletedListCount() => GetNumberOfJobsByStateName(States.DeletedState.StateName);
+
+        private long GetNumberOfJobsByStateName(string state)
         {
-            throw new NotImplementedException();
+            QueryBuilder builder = QueryBuilder.New($@"equalTo=""{state}""");
+            builder.OrderBy("state_name");
+            builder.Shallow(true);
+
+            FirebaseResponse response = connection.Client.Get("jobs");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Dictionary<string, bool> jobs = response.ResultAs<Dictionary<string, bool>>();
+                return jobs.LongCount();
+            }
+
+            return default(long);
         }
 
-        public long ProcessingCount()
+        public IDictionary<DateTime, long> SucceededByDatesCount() => GetDatesTimelineStats("succeeded");
+
+        public IDictionary<DateTime, long> FailedByDatesCount() => GetDatesTimelineStats("failed");
+
+        public IDictionary<DateTime, long> HourlySucceededJobs() => GetHourlyTimelineStats("succeeded");
+
+        public IDictionary<DateTime, long> HourlyFailedJobs() => GetHourlyTimelineStats("failed");
+
+        private Dictionary<DateTime, long> GetHourlyTimelineStats(string type)
         {
-            throw new NotImplementedException();
+            List<DateTime> dates = Enumerable.Range(0, 24).Select(x => DateTime.UtcNow.AddHours(-x)).ToList();
+            Dictionary<string, DateTime> keys = dates.ToDictionary(x => $"stats:{type}:{x:yyyy-MM-dd-HH}", x => x);
+            return GetTimelineStats(keys);
         }
 
-        public long SucceededListCount()
+        private Dictionary<DateTime, long> GetDatesTimelineStats(string type)
         {
-            throw new NotImplementedException();
+            List<DateTime> dates = Enumerable.Range(0, 7).Select(x => DateTime.UtcNow.AddDays(-x)).ToList();
+            Dictionary<string, DateTime> keys = dates.ToDictionary(x => $"stats:{type}:{x:yyyy-MM-dd}", x => x);
+            return GetTimelineStats(keys);
         }
 
-        public long DeletedListCount()
+        private Dictionary<DateTime, long> GetTimelineStats(Dictionary<string, DateTime> keys)
         {
-            throw new NotImplementedException();
+            Dictionary<DateTime, long> result = new Dictionary<DateTime, long>();
+            Parallel.ForEach(keys.Keys, key =>
+            {
+                FirebaseResponse response = connection.Client.Get($"counters/aggregrated/{key}");
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Counter counter = response.ResultAs<Counter>();
+                    DateTime date = keys.Where(k => k.Key == key).Select(k => k.Value).First();
+                    result.Add(date, counter.Value);
+                }
+            });
+            return result;
         }
 
-        public IDictionary<DateTime, long> SucceededByDatesCount()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IDictionary<DateTime, long> FailedByDatesCount()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IDictionary<DateTime, long> HourlySucceededJobs()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IDictionary<DateTime, long> HourlyFailedJobs()
-        {
-            throw new NotImplementedException();
-        }
+        #endregion
     }
 }
